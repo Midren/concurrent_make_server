80eef97601b4ea72e33048a325ad7481dde231a5
Kolinko-Danylo <kolinko@ucu.edu.ua>
2019-06-12 00:03:28 +0000

    Added POST, fixed PUT, refactored

--- app/routes.py
+++ app/routes.py
@@ -18,6 +18,20 @@ def view_helper():
         objs.append(obj)
     return objs
 
+def get_computer(form):
+    cur_compiler = Compiler.query.filter_by(compiler_name=form["compiler_name"]).filter_by(major_version=int(form["major_version"])).filter_by(minor_version=int(form["minor_version"])).first()
+
+    if cur_compiler is None:
+        cur_compiler = Compiler(compiler_name=form["compiler_name"], major_version=int(form["major_version"]), minor_version=int(form["minor_version"]))
+    cur_os = Os.query.filter_by(os_name=form["os_name"]).first()
+    if cur_os is None:
+        cur_os = Os(os_name=form["os_name"])
+    cur_computer = Computer.query.filter_by(compiler_id = cur_compiler.compiler_id, os_id=cur_os.os_id).first()
+    if cur_computer is None:
+        cur_computer = Computer(compiler_name = cur_compiler, os_name=cur_os)
+    return cur_computer
+
+
 @app.route("/node_summary", methods=["GET"])
 def get_summary():
     objs = view_helper()
@@ -27,50 +41,38 @@ def get_summary():
 @nodes.route("/", methods=["POST"])
 def create_node():
     form = request.get_json()
+    print(form)
     public_key = form["public_key"]
     ip = form["ip"]
     login = form["login"]
     user = User.query.filter_by(user_name=login).first()
     if not user:
         user = User(user_name=login)
-        db.session.add(user)
-        db.session.commit()
-    user = User.query.filter_by(user_name=login).first()
     user_list = Node.query.filter_by(public_key=public_key).all()
     if not len(user_list):
         node = Node(public_key=public_key, ip=ip, login=user)
-        db.session.add(node)
-        db.session.commit()
     else:
-        user_list[0].ip = ip
-        user_list[0].login = user
-        db.session.commit()
+        return "Data is already posted", 409
+
+    node.node_computer = get_computer(form)
+    db.session.add(node)
+    db.session.commit()
     return "Posted", 200
 
 
-@nodes.route("/<int:id>", methods=["PUT"])
-def change_node(id):
+@nodes.route("/", methods=["PUT"])
+def change_node():
     new_node = request.get_json()
-    node = Node.query.get(id)
+    node = Node.query.filter_by(public_key=new_node["public_key"]).first()
     if (node is None):
         return "Node is not found", 404
-    node.public_key = new_node["public_key"]
     node.ip = new_node["ip"]
     cur_user = User.query.filter_by(user_name=new_node["login"]).first()
     if cur_user is None:
         cur_user = User(user_name=new_node["login"])
     node.login = cur_user
-    cur_compiler = Compiler.query.filter_by(compiler_name=new_node["compiler_name"]).filter_by(major_version=int(new_node["major_version"])).filter_by(minor_version=int(new_node["minor_version"])).first()
 
-    if cur_compiler is None:
-        cur_compiler = Compiler(compiler_name=new_node["compiler_name"], major_version=int(new_node["major_version"]), minor_version=int(new_node["minor_version"]))
-    cur_os = Os.query.filter_by(os_name=new_node["os_name"]).first()
-    if cur_os is None:
-        cur_os = Os(os_name=new_node["os_name"])
-    cur_computer = Computer.query.filter_by(compiler_id = cur_compiler.compiler_id, os_id=cur_os.os_id).first()
-    if cur_computer is None:
-        cur_computer = Computer(compiler_name = cur_compiler, os_name=cur_os)
-    node.node_computer = cur_computer
+    node.node_computer = get_computer(new_node)
     db.session.commit()
     return jsonify(eval(str(new_node))), 200
 
